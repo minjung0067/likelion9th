7d92399e15cb2326aafcd12d8368461b1d771891
minjung <minjung0067@naver.com>
2021-05-20 13:13:44 +0900

    add proj

---
+++ firstproj/firstapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    objects = models.Manager()
+    title = models.CharField(max_length=100)
+    writer = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    body=models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ firstproj/firstapp/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from django.utils import timezone
+
+from .models import Blog
+
+# Create your views here.
+
+def main(request):
+    blogs = Blog.objects.all()
+    context = {
+        'blogs' : blogs
+    }
+    return render(request, 'main.html', context)
+
+def detail(request, id):
+    detail_data = get_object_or_404(Blog, pk=id)
+    context = {
+        'title' : detail_data.title,
+        'writer' : detail_data.writer,
+        'body' : detail_data.body,
+        'pub_date' : detail_data.pub_date,
+        'id' : id,
+    }
+    return render(request, 'detail.html', context)
+
+def create_page(request):
+    return render(request, 'create.html')
+
+def create(request):
+    new_data = Blog()
+    new_data.title = request.POST['title']
+    new_data.writer = request.POST['writer']
+    new_data.body = request.POST['body']
+    new_data.pub_date = timezone.now()
+    new_data.save()
+    return redirect('main')
+
+def update_page(request, id):
+    update_data = get_object_or_404(Blog, pk=id)
+    context = {
+        'id' : id,
+        'title' : update_data.title,
+        'writer' : update_data.writer,
+        'body' : update_data.body,
+    }
+    return render(request, 'update.html', context)
+
+def update(request, id):
+    update_data = get_object_or_404(Blog, pk=id)
+    update_data.title = request.POST['title']
+    update_data.writer = request.POST['writer']
+    update_data.body = request.POST['body']
+    update_data.pub_date = timezone.now()
+    update_data.save()
+    return redirect('main')
+
+def delete(request, id):
+    delete_data = get_object_or_404(Blog, pk=id)
+    delete_data.delete()
+    return redirect('main')
